a26f72e7bdc6f2def73a8024c054c7e29e45233d
M1urray <rknjonjo@gmail.com>
2023-05-24 10:35:33 +0300

    fix insland perimeter errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,24 +6,19 @@
 def island_perimeter(grid):
     if not grid:
         return 0
-
     rows = len(grid)
-    cols = len(grid[0])
     perimeter = 0
-
     for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Start with the assumption that each land cell contributes 4 to the perimeter
-
-                # Check adjacent cells and subtract the shared sides
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1  # Top cell is land, so subtract one side
-                if row < rows - 1 and grid[row + 1][col] == 1:
-                    perimeter -= 1  # Bottom cell is land, so subtract one side
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1  # Left cell is land, so subtract one side
-                if col < cols - 1 and grid[row][col + 1] == 1:
-                    perimeter -= 1  # Right cell is land, so subtract one side
-
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                row == 0 or (len(grid[row - 1]) > j and grid[row - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                row == rows - 1 or (len(grid[row + 1]) >
+                                 j and grid[row + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
