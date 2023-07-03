a5a088b187f67e76388f7f6f2a0c9991f3e14203
M1urray <rknjonjo@gmail.com>
2023-07-03 08:02:39 +0300

    fix errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
+"""
+Module used to rotate a matrix 90 degrees
+"""
+
+
 def rotate_2d_matrix(matrix):
-    n = len(matrix)
+    """
+    Given an n x n 2D matrix, rotate it 90 degrees.
+    Do not return anything. The matrix must be edited in-place.
+    You can assume the matrix will have 2 dim and will not be empty.
 
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    Arguments
+    ---------
+    - matrix  : matrix
+                list of non-negative integers.
 
-    # Reverse each row
-    for i in range(n):
-        matrix[i] = matrix[i][::-1]
+    Returns
+    -------
+    - Nothing. The function modifies the original argument
+    """
+
+    # create a replica of the original matrix
+    copy = matrix[:]
+
+    for i in range(len(matrix)):
+
+        # extract the i column from the copy
+        col_i = [row[i] for row in copy]
+
+        # place it on the original matrix in reverse order
+        matrix[i] = col_i[::-1]
